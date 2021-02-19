26e4d7f82b85b64220f3b9aa3a5a29c1b757a8ce
coffmanjrp <coffmanjrp@gmail.com>
2021-02-19 05:47:17 -0800

    Add links to Navbar

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = ({ title }) => {
   return (
-    <div className="navbar">
-      <h1>{title}</h1>
-    </div>
+    <>
+      <div className="navbar">
+        <h1>{title}</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
